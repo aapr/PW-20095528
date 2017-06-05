715c9f6ac1bd938ee7a846bccadf8485d40c2cb9
Arnulfo Paulino <aguateam@gmail.com>
2017-06-05 18:24:57 -0400

    now using express & response to index.html

--- webass.js
+++ webass.js
@@ -1,3 +1,7 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
 require('net').createServer(function(socket){
 	socket.on('data', function(data){
 		console.log(data.toString());
@@ -5,5 +9,10 @@ require('net').createServer(function(socket){
 
 	socket.write('HTTP/1.1 200 OK\n\n')
 	socket.end();
+
+	app.get('/', function(req, res){
+		res.sendfile(path.join(__dirname + '/index.html'));
+	});
+
 
 }).listen(5528);
