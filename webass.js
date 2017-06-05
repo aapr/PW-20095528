9ce872494969a695694ca6e632e33a9280e89934
Arnulfo Paulino <aguateam@gmail.com>
2017-06-05 20:58:45 +0000

    base app

--- webass.js
+++ webass.js
@@ -1,11 +1,9 @@
-require('net').createServer( function(socket){
+require('net').createServer(function(socket){
 	socket.on('data', function(data){
 		console.log(data.toString());
 	});
-	socket.write('http/1.1 200 OK \n\n OK')
-	socket.end();
-})
 
-.listen(8080,function(){
-	console.log('OK');
-})
+	socket.write('HTTP/1.1 200 OK\n\n')
+	socket.end();
+
+}).listen(5528);
